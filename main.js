e349334eb40a4965ca37f34f3e0855b38cb1b3d3
unknown <Jet>
2022-02-24 01:06:27 +0800

    add button and function

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // alert('it works')
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+const button = document.createElement("button")
+button.innerText = 'Go to top'
+button.style = 'position:sticky;top:0;z-index:10;'
+button.onclick = goToTop
+const header = document.querySelector('.dl_header')
+
+insertAfter(button, header)
+
+function goToTop(){
+    window.scrollTo(0, 0);
+}
